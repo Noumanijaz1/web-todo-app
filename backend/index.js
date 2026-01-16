793a376ff5c5ad9c554639054c7acf97c36cff76
Noumanijaz1 <dev.noumanijaz@gmail.com>
2026-01-16 12:12:54 +0500

    add the gitignore in the root

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
